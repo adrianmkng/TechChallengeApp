40c0a421d941c44717b7107940bf5c2da0d19e8b
Adrian Ng <n9.adrian@gmail.com>
2020-09-13 15:43:26 +1000

    Create systemd service for techchallenge app.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
 stack_name=techchallenge-stack 
 
-if ! aws cloudformation describe-stacks --region $AWS_DEFAULT_REGION --stack-name $stack_name
-  aws cloudformation create-stack --stack-name $stack_name --template-body file://deploy/techchallenge-cf.json --parameters ParameterKey="BuildBucket",ParameterValue="${BUILD_BUCKET}" ParameterKey="DBUser",ParameterValue="${DB_USER}" ParameterKey="DBPassword",ParameterValue="${DB_PASSWORD}"
+if ! aws cloudformation describe-stacks --region $AWS_DEFAULT_REGION --stack-name $stack_name; then
+  aws cloudformation create-stack --stack-name $stack_name --template-body file://techchallenge-cf.json --parameters ParameterKey="BuildBucket",ParameterValue="${BUILD_BUCKET}" ParameterKey="DBUser",ParameterValue="${DB_USER}" ParameterKey="DBPassword",ParameterValue="${DB_PASSWORD}"
 else
-  aws cloudformation update-stack --stack-name $stack_name --template-body file://deploy/techchallenge-cf.json --parameters ParameterKey="BuildBucket",ParameterValue="${BUILD_BUCKET}" ParameterKey="DBUser",ParameterValue="${DB_USER}" ParameterKey="DBPassword",ParameterValue="${DB_PASSWORD}"
+  aws cloudformation update-stack --stack-name $stack_name --template-body file://techchallenge-cf.json --parameters ParameterKey="BuildBucket",ParameterValue="${BUILD_BUCKET}" ParameterKey="DBUser",ParameterValue="${DB_USER}" ParameterKey="DBPassword",ParameterValue="${DB_PASSWORD}"
+fi
