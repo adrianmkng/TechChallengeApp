93392287ae24fecbb00cd5b0c8b9faafb8a88863
Adrian Ng <n9.adrian@gmail.com>
2020-09-14 21:23:49 +1000

    Use autoscaling group to deploy baked AMI.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+set -x
+
 stack_name=techchallenge-stack 
+ami_id=$1
 
 if ! aws cloudformation describe-stacks --region $AWS_DEFAULT_REGION --stack-name $stack_name; then
-  aws cloudformation create-stack --stack-name $stack_name --template-body file://techchallenge-cf.json --parameters ParameterKey="BuildBucket",ParameterValue="${BUILD_BUCKET}" ParameterKey="DBUser",ParameterValue="${DB_USER}" ParameterKey="DBPassword",ParameterValue="${DB_PASSWORD}"
+  aws cloudformation create-stack --stack-name $stack_name --template-body file://techchallenge-cf.json --parameters ParameterKey="DBUser",ParameterValue="${DB_USER}" ParameterKey="DBPassword",ParameterValue="${DB_PASSWORD}" ParameterKey="AmiId",ParameterValue="${ami_id}"
 else
-  aws cloudformation update-stack --stack-name $stack_name --template-body file://techchallenge-cf.json --parameters ParameterKey="BuildBucket",ParameterValue="${BUILD_BUCKET}" ParameterKey="DBUser",ParameterValue="${DB_USER}" ParameterKey="DBPassword",ParameterValue="${DB_PASSWORD}"
+  aws cloudformation update-stack --stack-name $stack_name --template-body file://techchallenge-cf.json --parameters ParameterKey="DBUser",ParameterValue="${DB_USER}" ParameterKey="DBPassword",ParameterValue="${DB_PASSWORD}" ParameterKey="AmiId",ParameterValue="${ami_id}"
 fi
