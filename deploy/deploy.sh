b6089ede8dc61272eb9377376c3189abf9f6ee72
Adrian Ng <n9.adrian@gmail.com>
2020-09-17 07:52:23 +1000

    Split build and deploy files.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,7 +6,16 @@ stack_name=techchallenge-stack
 ami_id=$1
 
 if ! aws cloudformation describe-stacks --region $AWS_DEFAULT_REGION --stack-name $stack_name; then
-  aws cloudformation create-stack --stack-name $stack_name --template-body file://techchallenge-cf.json --parameters ParameterKey="DBUser",ParameterValue="${DB_USER}" ParameterKey="DBPassword",ParameterValue="${DB_PASSWORD}" ParameterKey="AmiId",ParameterValue="${ami_id}"
+  aws cloudformation create-stack \
+    --stack-name $stack_name \
+    --template-body file://techchallenge-cf.json \
+    --parameters ParameterKey="DBUser",ParameterValue="${DB_USER}" \
+      ParameterKey="DBPassword",ParameterValue="${DB_PASSWORD}" \
+      ParameterKey="AmiId",ParameterValue="${ami_id}"
 else
-  aws cloudformation update-stack --stack-name $stack_name --template-body file://techchallenge-cf.json --parameters ParameterKey="DBUser",ParameterValue="${DB_USER}" ParameterKey="DBPassword",ParameterValue="${DB_PASSWORD}" ParameterKey="AmiId",ParameterValue="${ami_id}"
+  aws cloudformation update-stack 
+    --template-body file://techchallenge-cf.json \
+    --parameters ParameterKey="DBUser",ParameterValue="${DB_USER}" \
+      ParameterKey="DBPassword",ParameterValue="${DB_PASSWORD}" \
+      ParameterKey="AmiId",ParameterValue="${ami_id}"
 fi
---
+++ doc/design.md
@@ -0,0 +1,3 @@
+# TechChallenge Deployment design
+
+
