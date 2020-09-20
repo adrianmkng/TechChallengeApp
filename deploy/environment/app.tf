3c75f760a21a79e3da1d560cac666d71cc549e76
Adrian Ng <n9.adrian@gmail.com>
2020-09-20 17:13:52 +1000

    Add elb for techchallenge app.

--- deploy/environment/app.tf
+++ deploy/environment/app.tf
@@ -31,8 +31,10 @@ resource "aws_autoscaling_group" "techchallenge" {
   max_size                  = 3
   min_size                  = 1
   desired_capacity          = 1
+  load_balancers            = [aws_elb.techchallenge.name]
   launch_template      {
     id = aws_launch_template.techchallenge.id
+    version = "$Latest"
   }
   vpc_zone_identifier       = module.network.private_subnets.*.id
 }
@@ -65,6 +67,28 @@ resource "aws_launch_template" "techchallenge" {
     name = aws_iam_instance_profile.ssm_instance_profile.name
   }
   user_data = base64encode(data.template_file.user_data.rendered)
+  vpc_security_group_ids = [aws_security_group.techchallenge_node_access.id]
+}
+
+resource "aws_security_group" "techchallenge_node_access" {
+  name        = "${var.name}-node-access"
+  description = "Node access for ${var.name}"
+  vpc_id      = module.network.vpc.id
+
+  ingress {
+    description = "ELB traffic access"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    security_groups = [aws_security_group.elb_access.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 data "template_file" "user_data" {
@@ -78,6 +102,51 @@ data "template_file" "user_data" {
     db_port     = module.database.port
     listen_host = "0.0.0.0"
     listen_port = "3000"
+  }
+}
+
+resource "aws_elb" "techchallenge" {
+  name            = var.name
+  subnets         = module.network.public_subnets.*.id
+  security_groups = [aws_security_group.elb_access.id]
+
+  connection_draining         = true
+  connection_draining_timeout = 30
+
+  listener {
+    instance_port     = 3000
+    instance_protocol = "HTTP"
+    lb_port           = 80
+    lb_protocol       = "HTTP"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:3000/"
+    interval            = 30
+  }
+}
+
+resource "aws_security_group" "elb_access" {
+  name        = "${var.name}-elb"
+  description = "ELB access for ${var.name}"
+  vpc_id      = module.network.vpc.id
+
+  ingress {
+    description = "Public HTTP traffic access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
